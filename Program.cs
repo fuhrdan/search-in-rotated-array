b665e2b223689abbd0a434c7bb57e4a1f3efdbfe
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-07-29 13:30:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//*****************************************************************************
+//** 33. Search in Rotated Sorted Array  leetcode                            **
+//** Simple loop to find where it was rotated, return -1 if not found  -Dan  **
+//*****************************************************************************
+
+int search(int* nums, int numsSize, int target) {
+    for(int i = 0; i < numsSize; i++)
+        if(nums[i]==target) return i;
+    return -1;
+}
